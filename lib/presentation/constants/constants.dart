0b7219e505fe4cc3324ecf0818803aa0aeefe209
Adarsh Pradhan <dev.apradhan1508@gmail.com>
2024-10-26 11:26:24 +0530

    second last web commit

--- lib/presentation/constants/constants.dart
+++ lib/presentation/constants/constants.dart
@@ -1,8 +1,4 @@
-
 import '../pages/homepage/core/config/app_image.dart';
-
-import '../pages/homepage/core/config/assets/app_image.dart';
-
 
 class Constants {
   static List<List<List<String>>> pricelist = [
@@ -39,7 +35,6 @@ class Constants {
   ];
   static List<int> pop = [0, 1, 2];
   static List<List<String>> packages = [
-<<<<<<< HEAD
     [
       "Silver",
       "\$0.00",
@@ -57,13 +52,6 @@ class Constants {
     ],
   ];
   static List<List<dynamic>> recclist = [
-
-  ["Silver", "\$0.00", "For most businesses that want to optimize web queries"],
-  ["Gold", "\$50.00", "For most businesses that want to optimize web queries"],
-  ["Diamond", "\$80.00", "For most businesses that want to optimize web queries"],
-];
- static List<List<dynamic>> recclist = [
->>>>>>> 730c1692f04a0dd4ce490e1b2b8111358507174f
     [AppImage.p1, "Great Quality!", "James Gouse", "Graphic Designer"],
     [AppImage.p2, "Amazing Work!", "Tiana Philips", "Photographer"],
     [AppImage.p3, "Great Quality!", "Talan Westervelt", "Buisness Man"],
